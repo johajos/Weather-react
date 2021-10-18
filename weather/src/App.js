50a44acf41d16871f23ec3b4bb337f933fef08e5
JJ <jos.joha@hotmail.com>
2021-10-18 12:19:26 +0200

    Backgrounds, dates and css
    *NEW*
    - Created a function for dates and times
    - Added simple css to the app
    - Changed the background images

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,15 +1,54 @@
 import React from "react";
+// weather api
 const api = {
   key: "13a7dfe63027b6bf6434c3e2e257b08f",
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
+// month, day and time
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  };
+
   return (
     <div className="app">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeHolder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
