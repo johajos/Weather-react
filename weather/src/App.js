dcf32ee77397992289b3d1bfc6061506468096ab
JJ <jos.joha@hotmail.com>
2021-10-18 12:25:01 +0200

    Update to app.js
    - fixed small syntax error
    - added a couple of divs to display temperature and weather

--- weather/src/App.js
+++ weather/src/App.js
@@ -37,7 +37,7 @@ function App() {
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
+    return `${day} ${date} ${month} ${year}`;
   };
 
   return (
@@ -49,6 +49,10 @@ function App() {
         <div className="location-box">
           <div className="location">New York City, US</div>
           <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15°c</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
